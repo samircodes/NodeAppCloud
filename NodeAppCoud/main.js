f0a2bc36d5476ad36f58314a44b58e81622dbacf
Samir Kumar <87854004+samircodes@users.noreply.github.com>
2022-06-29 00:11:14 +0530

    Update main.js
    updated the main.js files

--- NodeAppCoud/main.js
+++ NodeAppCoud/main.js
@@ -1,33 +1,30 @@
 import { BlobServiceClient } from '@azure/storage-blob';
 import openpgp from 'openpgp';
-import fs, { ReadStream } from 'fs';
-import * as readline from 'node:readline';
-import { Stream } from 'stream';
-import { chunk } from 'chunk';
+import fs from 'fs';
+import { Transform, Writable } from 'stream';
 import { Kafka } from 'kafkajs';
-import { Transform } from 'node:stream';
-// import { disconnect } from 'process';
-
-
 
 (async() => {
-    
+
+   // code  for connecting to kafka cloud using Sasl authentication
     const kafka = new Kafka({
    
         ssl: true,
-        brokers: ["pkc-epwny.eastus.azure.confluent.cloud:9092"],
+        brokers: ["pkc-l7pr2.ap-south-1.aws.confluent.cloud:9092"],
         clientId: 'my-app',
         sasl: {
             mechanism: 'plain', // scram-sha-256 or scram-sha-512
-            username: "WWURDQN22AXUSOJU",
-            password: "2ZZxhaZY57/VEZSl6YT7RaAt3uj7DbVR990g0a+pwPeSYNfVobimj5cENfzKrlgx"
+            username: "NHVCNS5ENWZ2PJLI",                                                             // sasl username
+            password: "NWdaBXYNUFnrXMVPfZXq5aoYwkqNGaORUfRjZrR3BcbM46uxYi8LC1Ic9zFl3CJE"              // sasl password
           },
           connectionTimeout: 3000
-    
+           
       })
    
    
-    //const stream = require('stream');
+
+    // code for for downloading csv file from blob storage and decrypting it
+
     const AZURE_STORAGE_CONNECTION_STRING = 'AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;'
 
     const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
@@ -35,7 +32,9 @@ import { Transform } from 'node:stream';
     const containerName = 'files';
     const containerClient = blobServiceClient.getContainerClient(containerName);
 
-    const blobName = 'gap_uat_profile_extract_huge_2021053111050202.csv.pgp';
+    const blobName = 'gap_uat_profile_extract_2022042001000203.csv.gpg';
+
+   
     const blobClient = containerClient.getBlockBlobClient(blobName);
 
     const encryptedDataStream = (await blobClient.download(0)).readableStreamBody;
@@ -58,63 +57,58 @@ import { Transform } from 'node:stream';
         config: {allowUnauthenticatedStream: true}
     });
 
+    decryptedData.data.highWaterMark = 10000;
+
+ 
+      const producer = kafka.producer();
+      await producer.connect();
+       console.log("producer connected");
   
+
+
+
+  // Implementing transform function 
+
+    const t = new Transform({objectMode: true, 
+    transform(chunk, encoding, done) {
+        let data = chunk.toString();
+        if (this._lastLineData)
+        data = this._lastLineData + data;
+
+        let lines = data.split('\n');
+        this._lastLineData = lines.splice(lines.length-1, 1)[0];
+
+        lines.forEach(this.push.bind(this));
+        done();
+    },
+    flush(done){
+        if (this._lastLineData) 
+        this.push(this._lastLineData)
+        this._lastLineData = null;
+        done();
+   }, highWaterMark: 10000});
+
+  // Using writable stream to produce messages
+
+    const w = new Writable({
+        write: async(chunk, encoding, done) => {
+            await producer.send({
+                topic: 'AwsTopic',
+                messages: [{value: chunk}]
+            });
+            done();
+        }, highWaterMark: 10000
+    });
+
+    decryptedData.data.pipe(t).pipe(w);
+    
+    t.on('end', () => {
+        process.exit(1);
+    });
 
     
-   // code starts for producer
-   const producer = kafka.producer();
-   await producer.connect();
-   console.log("Producer connected");
-
-
-   var liner = new Transform({objectmode: true});
-  
-   liner._transform = function (chunk, encoding, done) {
-    var data = chunk.toString();
-    if (this._lastLineData) data = this._lastLineData + data;
-
-    var lines = data.split('\n');
-   
-   
-    this._lastLineData = lines.splice(lines.length-1,1)[0];
-
-    lines.forEach(this.push.bind(this));
-  
-    var line;
-    while (null !== (line = liner.read())) {
-        // do something with line
-        producer.send({
-            topic: 'topic3',
-            messages: [
-              { value: line,
-               partition:  0,
-                   },
-            ],
-          })
-   }
-    done();
-};
-liner._flush = function (done) {
-    if (this._lastLineData) this.push(this._lastLineData);
-    this._lastLineData = null;
-    var line;
-    while (null !== (line = liner.read())) {
-        // do something with line
-        producer.send({
-            topic: 'topic3',
-            messages: [
-              { value: line,
-               partition:  0,
-                   },
-            ],
-          })
-        }
-    done();
-};
-      
-    decryptedData.data.pipe(liner);
-
-    
-    // await producer.disconnect();
-
 })();
+
+
+
+
